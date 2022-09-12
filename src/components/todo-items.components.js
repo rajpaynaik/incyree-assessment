efa13e30adfed04e0502e116fb874d678801fef9
Raj Paynaik <rajpaynaik18@gmail.com>
2022-09-12 15:53:02 -0400

    initial commit

---
+++ src/components/todo-items.components.js
@@ -0,0 +1,15 @@
+// const TodoItems = (list) => {
+//   return (
+//     <div className="todo-list">
+//       {list.map((item) => (
+//         <div className="items-list">
+//           <input type="checkbox" className="input-checkbox" />
+//           <label key={item.id}>{item.name}</label>
+//           <button>delete</button>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default TodoItems
